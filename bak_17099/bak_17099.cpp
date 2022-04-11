b4c8ece1584f220337d95b5c9e8a00c7f7b041ec
kimcharless <89509933+kimcharless@users.noreply.github.com>
2022-04-11 23:51:24 +0900

    깔끔

--- bak_17099/bak_17099.cpp
+++ bak_17099/bak_17099.cpp
@@ -18,31 +18,36 @@ int main(){
     }
     cout<<Dyn[0];
 }
-
+// 우선순위큐 시간초과 실패
 // #include <bits/stdc++.h>
-// #define fastio cin.tie(0)->sync_with_stdio(0)
 // using namespace std;
+// struct contest{int s;int e;int val;};
+// int main(){
+//     int N,temp,iter=0,MAX = 0;
+//     pair<int,int> ans = {0,0};
+//     scanf("%d",&N);
+//     priority_queue<pair<int,int> , vector<pair<int,int>> ,greater<pair<int,int>>> next;
+//     vector<contest> con(N);
+//     for(int i=0;i<N;i++){scanf("%d %d %d",&con[i].s,&con[i].e,&con[i].val);}
+//     sort(con.begin(),con.end(),[](const contest &a,const contest &b){return a.s ^ b.s ? a.s < b.s : a.e < b.e;});
+//     for(auto K:con){
+//         if(next.empty()){
+//             next.push({K.e,K.val + ans.second});
+//             ans.first = K.s;    
+//         }
+//         else{
+//             while(next.top().first < K.s){
+//                 ans = {next.top().first, max(next.top().second , ans.second)};
+//                 next.pop();
+//             }
+//             ans.first = K.s;
+//             next.push({K.e , ans.second + K.val});
+//         }
+//     }
+//     while(!next.empty()){
+//         ans.second = max(ans.second , next.top().second);
+//         next.pop();
 
-// struct Info { int s, e, val; };
-
-// int main() {
-// 	fastio;
-// 	int n; cin >> n;
-// 	vector<Info> I(n);
-// 	for (auto& [s, e, val] : I) cin >> s >> e >> val;
-// 	sort(I.begin(), I.end(), [](const Info& a, const Info& b) { return a.s ^ b.s ? a.s < b.s : a.e < b.e; });
-
-// 	vector<int> DP(n);
-// 	for (int i = n - 1; i >= 0; i--) {
-// 		DP[i] = I[i].val;
-// 		if (i != n - 1) DP[i] = max(DP[i], DP[i + 1]);
-// 		auto it = upper_bound(I.begin(), I.end(), Info{ I[i].e, -1, -1 }, [](const Info& a, const Info& b) { return a.s < b.s; });
-// 		if (it != I.end()) DP[i] = max(DP[i], I[i].val + DP[it - I.begin()]);
-//         for(auto K:DP){
-//             cout.width(2);
-//             cout<<K;
-//         }cout<<endl;
-//         cout<<endl;
-// 	}
-// 	cout << DP[0] << '\n';
+//     }
+//     cout<<ans.second;
 // }
